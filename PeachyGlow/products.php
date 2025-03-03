7737fd9abc9e821dd8c4f20c4e5c116ab5405b3f
Thipnaree02 <65010914602@msu.ac.th>
2025-03-03 13:54:16 +0700

    a

--- PeachyGlow/products.php
+++ PeachyGlow/products.php
@@ -155,13 +155,6 @@
                   </div>
                </div>
          </div>
-         <div class="coffee_section layout_padding">
-         <div class="container">
-            <div class="row">
-               <h1 class="coffee_taital">OUR Coffee OFFER</h1>
-               <div class="bulit_icon"><img src="images/bulit-icon.png"></div>
-            </div>
-         </div>
          <div class="coffee_section_2">
             <div id="main_slider" class="carousel slide" data-ride="carousel">
                <div class="carousel-inner">
