581591ffaea9a7deececdf7eb8a4017d9fce64ec
Thipnaree02 <65010914602@msu.ac.th>
2025-03-03 14:20:02 +0700

    a

--- PeachyGlow/products.php
+++ PeachyGlow/products.php
@@ -139,7 +139,6 @@
                         </div>
                      </div>
                   </div>
-                  <div class="carousel-item">
                      <div class="container-fluid">
                         <div class="row">
                            <div class="col-lg-3 col-md-6">
