19a1017b7c78c785d4857e8af154b4db15550148
Thipnaree02 <65010914602@msu.ac.th>
2025-03-03 15:35:25 +0700

    a

--- PeachyGlow/products.php
+++ PeachyGlow/products.php
@@ -238,5 +238,27 @@
       <!-- sidebar -->
       <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
       <script src="js/custom.js"></script>
+
+      <h1> insert product Form </h1>
+<form method = "post" action="">
+    Product Name <input type="text" name="pname" required autofocus <br>
+    Product Price <input type="text" name="pprice" required <br>
+    <button type="submit">Save</button>
+</form>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    include_once("../connectdb.php");
+    $pname = $_POST['pname'];
+    $pprice = $_POST['pprice'];
+    $sql = "INSERT INTO products (p_name, p_price) VALUES ('{$pname}','{$pprice}')";
+    mysqli_query($conn, $sql) or die ('insert error');
+    
+    echo "<script>";
+    echo "alert('success!');";
+    echo "</script>";
+    
+}
+?>
    </body>
 </html>
