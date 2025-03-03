bada33e1eb769a8020833bffb753bc7d013258e9
Thipnaree02 <65010914602@msu.ac.th>
2025-03-03 13:46:30 +0700

    a

--- PeachyGlow/products.php
+++ PeachyGlow/products.php
@@ -111,67 +111,6 @@
                         </div>
                      </div>
                   </div>
-                  <div class="carousel-item">
-                     <div class="container-fluid">
-                        <div class="row">
-                           <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/img-1.png"></div>
-                              <h3 class="types_text">TYPES OF COFFEE</h3>
-                              <p class="looking_text">looking at its layout. The point of</p>
-                              <div class="read_bt"><a href="#">Read More</a></div>
-                           </div>
-                           <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/img-2.png"></div>
-                              <h3 class="types_text">BEAN VARIETIES</h3>
-                              <p class="looking_text">looking at its layout. The point of</p>
-                              <div class="read_bt"><a href="#">Read More</a></div>
-                           </div>
-                           <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/img-3.png"></div>
-                              <h3 class="types_text">COFFEE & PASTRY</h3>
-                              <p class="looking_text">looking at its layout. The point of</p>
-                              <div class="read_bt"><a href="#">Read More</a></div>
-                           </div>
-                           <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/img-4.png"></div>
-                              <h3 class="types_text">COFFEE TO GO</h3>
-                              <p class="looking_text">looking at its layout. The point of</p>
-                              <div class="read_bt"><a href="#">Read More</a></div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="carousel-item">
-                     <div class="container-fluid">
-                        <div class="row">
-                           <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/img-1.png"></div>
-                              <h3 class="types_text">TYPES OF COFFEE</h3>
-                              <p class="looking_text">looking at its layout. The point of</p>
-                              <div class="read_bt"><a href="#">Read More</a></div>
-                           </div>
-                           <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/img-2.png"></div>
-                              <h3 class="types_text">BEAN VARIETIES</h3>
-                              <p class="looking_text">looking at its layout. The point of</p>
-                              <div class="read_bt"><a href="#">Read More</a></div>
-                           </div>
-                           <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/img-3.png"></div>
-                              <h3 class="types_text">COFFEE & PASTRY</h3>
-                              <p class="looking_text">looking at its layout. The point of</p>
-                              <div class="read_bt"><a href="#">Read More</a></div>
-                           </div>
-                           <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/img-4.png"></div>
-                              <h3 class="types_text">COFFEE TO GO</h3>
-                              <p class="looking_text">looking at its layout. The point of</p>
-                              <div class="read_bt"><a href="#">Read More</a></div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
                </div>
          </div>
          <div class="coffee_section_2">
