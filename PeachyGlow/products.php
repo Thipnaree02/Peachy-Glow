5e5edba7ba2db2b98e65476e185a1c63841337ff
Thipnaree02 <65010914602@msu.ac.th>
2025-03-03 13:49:59 +0700

    a

--- PeachyGlow/products.php
+++ PeachyGlow/products.php
@@ -120,25 +120,25 @@
                      <div class="container-fluid">
                         <div class="row">
                            <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/1.png"></div>
+                              <div class="coffee_img"><img src="images/5.png"></div>
                               <h3 class="types_text">Olay Regenerist</h3>
                               <p class="looking_text">ครีมบำรุงผิวช่วยลดเลือนริ้วรอย เสริมสร้างความยืดหยุ่น และกระชับผิว ด้วยสูตรที่มีเปปไทด์และไนอาซินาไมด์</p>
                               <div class="read_bt"><a href="#">Read More</a></div>
                            </div>
                            <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/2.png"></div>
+                              <div class="coffee_img"><img src="images/6.png"></div>
                               <h3 class="types_text">Hada Labo</h3>
                               <p class="looking_text">โลชั่นและเซรั่มบำรุงผิวจากญี่ปุ่น เน้นการเติมความชุ่มชื้นล้ำลึกด้วยกรดไฮยาลูโรนิกหลายโมเลกุล</p>
                               <div class="read_bt"><a href="#">Read More</a></div>
                            </div>
                            <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/3.png"></div>
+                              <div class="coffee_img"><img src="images/7.png"></div>
                               <h3 class="types_text">Anessa</h3>
                               <p class="looking_text">กันแดดจากญี่ปุ่นที่มีประสิทธิภาพสูง ป้องกันรังสี UVA/UVB และกันน้ำได้ดี เหมาะสำหรับกิจกรรมกลางแจ้ง</p>
                               <div class="read_bt"><a href="#">Read More</a></div>
                            </div>
                            <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/4.png"></div>
+                              <div class="coffee_img"><img src="images/8.png"></div>
                               <h3 class="types_text">Mille</h3>
                               <p class="looking_text">แบรนด์เครื่องสำอางที่เน้นการแต่งหน้าแบบเบาบาง เนื้อเนียนนุ่ม ติดทนนาน และให้ลุคธรรมชาติ</p>
                               <div class="read_bt"><a href="#">Read More</a></div>
@@ -156,25 +156,25 @@
                      <div class="container-fluid">
                         <div class="row">
                            <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/1.png"></div>
+                              <div class="coffee_img"><img src="images/9.png"></div>
                               <h3 class="types_text">Olay Regenerist</h3>
                               <p class="looking_text">ครีมบำรุงผิวช่วยลดเลือนริ้วรอย เสริมสร้างความยืดหยุ่น และกระชับผิว ด้วยสูตรที่มีเปปไทด์และไนอาซินาไมด์</p>
                               <div class="read_bt"><a href="#">Read More</a></div>
                            </div>
                            <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/2.png"></div>
+                              <div class="coffee_img"><img src="images/10.png"></div>
                               <h3 class="types_text">Hada Labo</h3>
                               <p class="looking_text">โลชั่นและเซรั่มบำรุงผิวจากญี่ปุ่น เน้นการเติมความชุ่มชื้นล้ำลึกด้วยกรดไฮยาลูโรนิกหลายโมเลกุล</p>
                               <div class="read_bt"><a href="#">Read More</a></div>
                            </div>
                            <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/3.png"></div>
+                              <div class="coffee_img"><img src="images/11.png"></div>
                               <h3 class="types_text">Anessa</h3>
                               <p class="looking_text">กันแดดจากญี่ปุ่นที่มีประสิทธิภาพสูง ป้องกันรังสี UVA/UVB และกันน้ำได้ดี เหมาะสำหรับกิจกรรมกลางแจ้ง</p>
                               <div class="read_bt"><a href="#">Read More</a></div>
                            </div>
                            <div class="col-lg-3 col-md-6">
-                              <div class="coffee_img"><img src="images/4.png"></div>
+                              <div class="coffee_img"><img src="images/12a.png"></div>
                               <h3 class="types_text">Mille</h3>
                               <p class="looking_text">แบรนด์เครื่องสำอางที่เน้นการแต่งหน้าแบบเบาบาง เนื้อเนียนนุ่ม ติดทนนาน และให้ลุคธรรมชาติ</p>
                               <div class="read_bt"><a href="#">Read More</a></div>
