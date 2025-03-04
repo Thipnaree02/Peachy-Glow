0453781e2c4aac8291c4c3f6bf5b634cacc88cac
Thipnaree02 <65010914602@msu.ac.th>
2025-03-04 23:05:14 +0700

    a

--- Peachy-Glow/all-products/about.php
+++ Peachy-Glow/all-products/about.php
@@ -202,6 +202,27 @@ https://templatemo.com/tm-571-hexashop
                                     </ul>
                                 </div>
                             </div>
+                            <img src="assets/images/team-member-02.jpg">
+                        </div>
+                        <div class="down-content">
+                            <h4>Ragnar Lodbrok</h4>
+                            <span>Product Caretaker</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="team-item">
+                        <div class="thumb">
+                            <div class="hover-effect">
+                                <div class="inner-content">
+                                    <ul>
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
                             <img src="assets/images/team-member-03.jpg">
                         </div>
                         <div class="down-content">
