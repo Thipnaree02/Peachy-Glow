fe67c2ba7ed77dad4e2acabb22da662c7a4d4ae4
Thipnaree02 <65010914602@msu.ac.th>
2025-03-10 14:56:21 +0700

    a

--- Peachy-Glow/all-products/cart.php
+++ Peachy-Glow/all-products/cart.php
@@ -8,6 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
     <title>PeachyGlow - Contact Page</title>
 
